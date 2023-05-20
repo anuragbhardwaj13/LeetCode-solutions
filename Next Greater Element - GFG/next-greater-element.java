3007ccd829ed21ab983e58f35d1fc4ff289be88b
Anurag Bhardwaj <65033601+anuragbhardwaj13@users.noreply.github.com>
2023-05-20 15:44:35 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.java
+++ Next Greater Element - GFG/next-greater-element.java
@@ -35,29 +35,19 @@ class Solution
     public static long[] nextLargerElement(long[] arr, int n)
     { 
         // Your code here
-        Stack<Long> st=new Stack<>();
-        long[] res=new long[n];
+        Stack<Long> stack=new Stack<>();
+        long[] nge=new long[n];
         for(int i=n-1;i>=0;i--){
-            if(st.isEmpty()){
-                res[i]=-1;                
+            while(!stack.isEmpty()&&stack.peek()<=arr[i]){
+                stack.pop();
             }
-            else{
-                if(st.peek()>arr[i]){
-                    res[i]=st.peek();
-                }else{
-                    while(!st.isEmpty()&&st.peek()<=arr[i]){
-                        st.pop();
-                    }
-                    if(st.isEmpty()){
-                        res[i]=-1;
-                    }else{
-                       res[i]=st.peek();
-   
-                    }
-                }
+            if(stack.isEmpty()){
+                nge[i]=-1;
+            }else{
+                nge[i]=stack.peek();
             }
-            st.push(arr[i]);
+            stack.push(arr[i]);
         }
-        return res;
+        return nge;
     } 
 }
