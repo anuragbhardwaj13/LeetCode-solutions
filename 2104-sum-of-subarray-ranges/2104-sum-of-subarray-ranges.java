048bf35923fa130bd40f50a2fbc5e8a00f9c3f6d
Anurag Bhardwaj <65033601+anuragbhardwaj13@users.noreply.github.com>
2023-05-20 20:13:12 +0530

    Time: 38 ms (8.16%), Space: 45.2 MB (5.31%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -0,0 +1,81 @@
+class Solution {
+    public long subArrayRanges(int[] nums) {
+                long sum=0;
+    int n=nums.length;
+    Stack<Integer> st=new Stack<>();
+    int[] prevMin=new int[n];
+    int[] prevMax=new int[n];
+    int[] nextMin=new int[n];
+    int[] nextMax=new int[n];
+    Arrays.fill(prevMin , -1);
+    Arrays.fill(nextMin , n);
+    Arrays.fill(prevMax , -1 );
+    Arrays.fill(nextMax , n);
+    //calculate left min
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty()&&nums[st.peek()]>=nums[i]){
+                st.pop();
+            }
+            if(!st.isEmpty()){
+                prevMin[i]=st.peek();
+            }
+            st.push(i);
+        }
+        //empty stack here
+        while(!st.isEmpty()){
+            st.pop();
+        }
+    //calculate right min
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty()&&nums[st.peek()]>nums[i]){
+                st.pop();
+            }
+            if(!st.isEmpty()){
+                nextMin[i]=st.peek();
+            }
+            st.push(i);
+        }
+        //empty stack here
+        while(!st.isEmpty()){
+            st.pop();
+        }
+        //calculate left max
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty()&&nums[st.peek()]<=nums[i]){
+                st.pop();
+            }
+            if(!st.isEmpty()){
+                prevMax[i]=st.peek();
+            }
+            st.push(i);
+        }
+        //empty stack here
+        while(!st.isEmpty()){
+            st.pop();
+        }
+    //calculate right max
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty()&&nums[st.peek()]<nums[i]){
+                st.pop();
+            }
+            if(!st.isEmpty()){
+                nextMax[i]=st.peek();
+            }
+            st.push(i);
+        }
+        //empty stack here
+        while(!st.isEmpty()){
+            st.pop();
+        }
+        
+        for(int i=0;i<n;i++){
+            long lMin=i-prevMin[i];
+            long rMin=nextMin[i]-i;
+            long lMax=i-prevMax[i];
+            long rMax=nextMax[i]-i;
+            
+            sum+=(lMax*rMax-lMin*rMin)*nums[i];
+        }
+        return sum;
+    }
+}
