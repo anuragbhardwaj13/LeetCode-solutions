e8dac9c5d43f9312fe3ea5aa94529e1af9e58942
Anurag Bhardwaj <65033601+anuragbhardwaj13@users.noreply.github.com>
2023-05-23 00:20:40 +0530

    Added solution - LeetHub

---
+++ Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])
+    {
+        //taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+        
+        //taking total count of testcases
+        int t = sc.nextInt();
+        
+        
+        
+        while(t-- > 0)
+        {
+            //taking total number of elements
+            int n = sc.nextInt();
+            
+            //taking size of subArrays 
+            int k = sc.nextInt();
+            
+            //Declaring and Intializing an array of size n
+            int arr[] = new int[n];
+            
+            //adding all the elements to the array 
+            for(int i = 0; i <n; i++)
+            {
+                arr[i] =sc.nextInt();
+            }
+            
+            //Calling the method max_of_subarrays of class solve
+            //and storing the result in an ArrayList
+            ArrayList <Integer> res = new Solution().max_of_subarrays(arr, n, k);
+            
+            //printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++)
+                System.out.print (res.get (i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    //Function to find maximum of each subarray of size k.
+    static ArrayList <Integer> max_of_subarrays(int nums[], int n, int k)
+    {
+        // Your code here
+        ArrayDeque<Integer> st=new ArrayDeque<>();
+        int left=0;
+        int right=0;
+        ArrayList<Integer> resList=new ArrayList<>();
+        while(right<n){
+            while(!st.isEmpty() && nums[st.getLast()]<nums[right]){
+                st.removeLast();
+            }
+            st.addLast(right);
+            if(left>st.getFirst()){
+                st.removeFirst();
+            }
+            if(right+1>=k){
+                resList.add(nums[st.getFirst()]);
+                left++;
+            }
+            right++;
+        }
+        
+        return resList;
+    }
+}
